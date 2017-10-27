2c18f2ffff06c8383bd090933f4abba1993ac769
RodrigoAGM <rod-gm@hotmail.com>
2017-10-27 03:47:42 -0500

    Add markers function implemented

--- Project/main.js
+++ Project/main.js
@@ -1,5 +1,24 @@
+
+function Place(latitude, longitude) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+}
+
+var PlacesArray = [];
+
 var map = new GMaps({
     el: '#map',
     lat: -12.043333,
-    lng: -77.028333
+    lng: -77.028333,
+
+    click: function(e) {
+        var latitude = e.latLng.lat();
+        var longitude = e.latLng.lng();
+        map.addMarker({
+            lat: latitude,
+            lng: longitude
+        });
+
+        PlacesArray.push(new Place(latitude,longitude));
+    }
 });
