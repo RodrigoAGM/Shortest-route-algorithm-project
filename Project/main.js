4062cc24639422e3a344ec20edefbda6e4332c30
RodrigoAGM <rod-gm@hotmail.com>
2017-10-27 16:03:28 -0500

    Distance calculation

--- Project/main.js
+++ Project/main.js
@@ -24,17 +24,23 @@ var map = new GMaps({
         PlacesArray.push(new Place(latitude,longitude));
         var pos = PlacesArray.length - 2;
 
-        if (PlacesArray.length >= 2){
+        if (PlacesArray.length >= 2) {
             map.drawRoute({
                 origin: [PlacesArray[pos].latitude, PlacesArray[pos].longitude],
-                destination: [PlacesArray[pos+1].latitude, PlacesArray[pos+1].longitude],
+                destination: [PlacesArray[pos + 1].latitude, PlacesArray[pos + 1].longitude],
                 travelMode: 'driving',
                 strokeColor: '#131540',
                 strokeOpacity: 0.6,
                 strokeWeight: 6
-            })
+            });
+            Distance();
         }
     }
 });
 
-
+function Distance(){
+    srcLocation = new google.maps.LatLng(PlacesArray[0].latitude, PlacesArray[0].longitude);
+    dstLocation = new google.maps.LatLng(PlacesArray[1].latitude, PlacesArray[1].longitude);
+    var distance = google.maps.geometry.spherical.computeDistanceBetween(srcLocation, dstLocation)
+    console.log('distance = ' + distance/1000); // Distance in Kms.
+}
