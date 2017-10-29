12d57f5619459f362dfca802c5e84b91e20f8184
RodrigoAGM <rod-gm@hotmail.com>
2017-10-29 01:34:04 -0500

    Distances matrix finished

--- Project/main.js
+++ Project/main.js
@@ -1,12 +1,53 @@
-
-
 var PlacesArray = [];
 var icon = "icon.png";
-
+var LatLngArray = [];
+var ArrayofArrays = [];
+var nElementos;
+var DistancesMatrix;
+var service = new google.maps.DistanceMatrixService();
 
 function Place(latitude, longitude) {
     this.latitude = latitude;
     this.longitude = longitude;
+}
+
+function BuildArrayofArrays() {
+
+    ArrayofArrays = [];
+
+    nElementos = PlacesArray.length/10;
+    var nEntero = parseInt(nElementos.toFixed());
+    //console.log(nEntero);
+
+    if (nElementos - nEntero > 0){
+        nElementos = nEntero+1;
+    }
+    else{
+        nElementos = nEntero;
+    }
+
+    //console.log('cant='+ nElementos);
+
+    var n = PlacesArray.length;
+    var limite = 0;
+
+    for(var i = 0; i < nElementos; ++i){
+
+        if(n-10 > 0){
+            limite = 10;
+            n-=10;
+        }
+        else{
+            limite = n;
+        }
+        //console.log('lim='+limite);
+        for(var j = 0; j < limite; ++j){
+            LatLngArray.push(new google.maps.LatLng(PlacesArray[j+(10*i)].latitude,PlacesArray[j+(10*i)].longitude));
+        }
+        ArrayofArrays.push(LatLngArray);
+        LatLngArray = [];
+    }
+    //console.log(ArrayofArrays);
 }
 
 var map = new GMaps({
@@ -40,7 +81,7 @@ var map = new GMaps({
     }
 });
 
-function Distance() {
+/**function Distance() {
     if (PlacesArray.length >= 2) {
 
         var DistanceMatrix = new Array(PlacesArray.length);
@@ -62,4 +103,73 @@ function Distance() {
         }
         console.log(DistanceMatrix);
     }
+}**/
+
+function Distance() {
+
+    if (PlacesArray.length >= 2) {
+
+        DistancesMatrix = new Array(PlacesArray.length);
+        for(var i = 0; i<PlacesArray.length; ++i){
+            DistancesMatrix[i] = new Array(PlacesArray.length);
+        }
+
+        BuildArrayofArrays();
+        //console.log(ArrayofArrays[0].length + "  "+ nElementos);
+        console.log(nElementos);
+        for(var k = 0; k < nElementos; ++k){
+            for(var l = 0; l < nElementos; ++l){
+
+            }//Fin for
+        }//Fin for
+    }//Fin if
+    console.log(DistancesMatrix);
+}
+
+var w=0;
+var r=0;
+
+function Step(){
+    console.log(w+'  '+r);
+    CalDistances(w,r);
+    if(r == nElementos-1) {
+        if (w != nElementos-1) {
+            w++;
+            r = 0;
+        }
+        else{
+            var btnStep = document.getElementById('btnStep');
+            btnStep.disabled =true;
+            console.log(DistancesMatrix);
+        }
+    }
+    else{r++;}
+
+}
+
+
+function CalDistances(k,l){
+
+    service.getDistanceMatrix(
+        {
+            origins: ArrayofArrays[k],
+            destinations: ArrayofArrays[l],
+            travelMode: 'DRIVING'
+        }, function callback(response, status) {
+            if (status == 'OK') {
+                var origins = response.originAddresses;
+                var destinations = response.destinationAddresses;
+
+                for (var i = 0; i < origins.length; i++) {
+                    var results = response.rows[i].elements;
+                    for (var j = 0; j < results.length; j++) {
+                        var element = results[j];
+                        var distance = element.distance.value;
+                       DistancesMatrix[i+(10*k)][j+(10*l)]=distance;
+                        //console.log((i+(10*k)) + '_' +(j+(10*l)));
+                    }
+                }
+            }
+        });
+
 }
